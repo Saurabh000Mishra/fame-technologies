8a1000c6f070eebaa27bb306c46934f8cc3bc196
Saurabh000Mishra <32061528+Saurabh000Mishra@users.noreply.github.com>
2019-01-05 14:44:45 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,28 @@
+public class string
+{ 
+    static final int ASCII_SIZE = 256; 
+    static char getMaxOccuringChar(String str) 
+    { 
+
+        int count[] = new int[];
+       int len = str.length(); 
+        for (int i=0; i<len; i++) 
+            count[str.charAt(i)]++; 
+       int max = -1;   
+        char result = ' ';    
+       for (int i = 0; i < len; i++) { 
+            if (max < count[str.charAt(i)]) 
+            { 
+                max = count[str.charAt(i)]; 
+                result = str.charAt(i); 
+            } 
+        } 
+       
+        return result; 
+    }  
+      public static void main(String[] args) 
+    { 
+        String str = "sample string"; 
+        System.out.println("Max occurring character is " + getMaxOccuringChar(str)); 
+    } 
+} 
